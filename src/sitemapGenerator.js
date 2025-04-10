05dbdb1c49504ee676434d8f65f21c2c707ea3f3
ked <kedric.me@gmail.com>
2025-04-10 10:30:31 +0900

    ssl 수정

--- src/sitemapGenerator.js
+++ src/sitemapGenerator.js
@@ -7,7 +7,7 @@ const Sitemap = require("react-router-sitemap").default;
 
 function generateSitemap() {
   return new Sitemap(router)
-    .build("http://candv.co.kr")
+    .build("https://candv.co.kr")
     .save("./public/sitemap.xml");
 }
 
