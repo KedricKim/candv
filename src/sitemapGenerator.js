aac13cb8fdb1f4a3b10aaac0be6323e8e2831cd4
ked <kedric.me@gmail.com>
2025-04-10 11:00:45 +0900

    재수정

--- src/sitemapGenerator.js
+++ src/sitemapGenerator.js
@@ -7,7 +7,7 @@ const Sitemap = require("react-router-sitemap").default;
 
 function generateSitemap() {
   return new Sitemap(router)
-    .build("https://candv.co.kr")
+    .build("https://www.candv.co.kr")
     .save("./public/sitemap.xml");
 }
 
